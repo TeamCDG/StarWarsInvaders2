b93d7db59cf912bdce4a4aa600d7404c0933fbd1
Josh Theis <cdg.josh@googlemail.com>
2013-03-21 03:03:04 +0100

    Created classes

---
+++ src/cdg/swi/game/menu/MenuFrame.java
@@ -0,0 +1,16 @@
+package cdg.swi.game.menu;
+
+import java.util.*;
+
+import cdg.swi.game.util.interfaces.IMenuObject;
+
+public class MenuFrame {
+
+	private List<IMenuObject> components;
+	
+	public void draw()
+	{
+		
+	}
+
+}
---
+++ src/cdg/swi/game/runnable/Main.java
@@ -0,0 +1,13 @@
+package cdg.swi.game.runnable;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
