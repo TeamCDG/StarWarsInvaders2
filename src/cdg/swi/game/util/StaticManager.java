63aae69e21968458a863b24db2bca1155a275f93
Josh Theis <cdg.josh@googlemail.com>
2013-03-24 02:58:50 +0100

    default clear color

--- src/cdg/swi/game/util/StaticManager.java
+++ src/cdg/swi/game/util/StaticManager.java
@@ -4,5 +4,6 @@ public abstract class StaticManager {
 
 	public static int SELECTION_SHADER_PROGRAM_ID = 0;
 	public static int MENU_SHADER_PROGRAM_ID = 0;
+	public static float[] CLEAR_COLOR = new float[]{0.0f,0.0f,0.0f,1.0f};
 
 }
