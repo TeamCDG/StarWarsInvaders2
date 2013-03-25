4c5967b962ef16f2573358cbdca30db7b7b3de6b
Josh Theis <cdg.josh@googlemail.com>
2013-03-25 04:02:20 +0100

    window height and window with

--- src/cdg/swi/game/util/StaticManager.java
+++ src/cdg/swi/game/util/StaticManager.java
@@ -4,6 +4,8 @@ public abstract class StaticManager {
 
 	public static int SELECTION_SHADER_PROGRAM_ID = 0;
 	public static int MENU_SHADER_PROGRAM_ID = 0;
-	public static float[] CLEAR_COLOR = new float[]{0.0f,0.0f,0.0f,1.0f};
+	public static float[] CLEAR_COLOR = new float[]{1.0f,0.0f,0.0f,1.0f};
+	public static int WINDOW_WIDTH = 600;
+	public static int WINDOW_HEIGHT = 600;
 
 }
