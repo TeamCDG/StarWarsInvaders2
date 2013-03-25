21d8f8e6139a1cb225653085f2a85c2eb52e8c34
Josh Theis <cdg.josh@googlemail.com>
2013-03-25 04:01:19 +0100

    main menu frame

---
+++ src/cdg/swi/game/menu/MainMenu.java
@@ -0,0 +1,14 @@
+package cdg.swi.game.menu;
+
+public class MainMenu extends MenuFrame 
+{
+
+	public MainMenu()
+	{
+		Button b = new Button(-0.5f, 0.2f);
+		this.add(b);
+		
+		Button b2 = new Button(0.0f, 0.0f);
+		this.add(b2);
+	}
+}
