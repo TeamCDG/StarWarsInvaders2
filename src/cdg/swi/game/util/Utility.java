a45ccaa5eef6d9386e31c4dadf9d8d51c1db6491
Josh Theis <cdg.josh@googlemail.com>
2013-03-25 04:01:55 +0100

    load shader method

--- src/cdg/swi/game/util/Utility.java
+++ src/cdg/swi/game/util/Utility.java
@@ -1,5 +1,12 @@
 package cdg.swi.game.util;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
+
 public abstract class Utility 
 {
 	
@@ -23,7 +30,8 @@ public abstract class Utility
 		else
 			col = new float[] { (float)val[3]*GL_COLOR_PER_BIT,
 								(float)val[2]*GL_COLOR_PER_BIT,
-								(float)val[1]*GL_COLOR_PER_BIT};
+								(float)val[1]*GL_COLOR_PER_BIT,
+								1.0f};
 		
 		return col;
 	}
@@ -49,5 +57,35 @@ public abstract class Utility
 		            (0 & 0xFF) << 24;
 		}
 		
+	}
+	
+	public static int loadShader(String filename, int type) 
+	{
+		StringBuilder shaderSource = new StringBuilder();
+		int shaderID = 0;
+		
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(filename));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				shaderSource.append(line).append("\n");
+			}
+			reader.close();
+		} catch (IOException e) {
+			System.err.println("Could not read file.");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		
+		shaderID = GL20.glCreateShader(type);
+		GL20.glShaderSource(shaderID, shaderSource);
+		GL20.glCompileShader(shaderID);
+		
+		if (GL20.glGetShader(shaderID, GL20.GL_COMPILE_STATUS) == GL11.GL_FALSE) {
+			System.err.println("Could not compile shader.");
+			System.exit(-1);
+		}
+		
+		return shaderID;
 	}
 }
