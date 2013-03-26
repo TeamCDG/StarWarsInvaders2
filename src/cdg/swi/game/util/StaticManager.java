7094349eb407d50af45c612daac9bbe3c8775b67
Josh Theis <cdg.josh@googlemail.com>
2013-03-26 05:33:37 +0100

    added font texture id

--- src/cdg/swi/game/util/StaticManager.java
+++ src/cdg/swi/game/util/StaticManager.java
@@ -7,5 +7,6 @@ public abstract class StaticManager {
 	public static float[] CLEAR_COLOR = new float[]{1.0f,0.0f,0.0f,1.0f};
 	public static int WINDOW_WIDTH = 600;
 	public static int WINDOW_HEIGHT = 600;
+	public static int FONT_TEXTURE_ID = 0;
 
 }
