53beb340b9e2dd1dee67261a36a66b7816710e4d
Josh Theis <cdg.josh@googlemail.com>
2013-03-26 05:32:02 +0100

    position of the font characters

---
+++ src/cdg/swi/game/util/FontFinals.java
@@ -0,0 +1,508 @@
+package cdg.swi.game.util;
+
+public abstract class FontFinals 
+{
+	public static final float A_X = 0.001953125f;
+	public static final float A_Y = 0.0263671875f;
+	public static final float A_WIDTH = 0.0732421875f;
+	public static final float A_HEIGHT = 0.072265625f;
+	
+	public static final float B_X = 0.1015625f;
+	public static final float B_Y = 0.0263671875f;
+	public static final float B_WIDTH = 0.0693359375f;
+	public static final float B_HEIGHT = 0.072265625f;
+	
+	public static final float C_X = 0.201171875f;
+	public static final float C_Y = 0.0263671875f;
+	public static final float C_WIDTH = 0.0625f;
+	public static final float C_HEIGHT = 0.072265625f;
+	
+	public static final float D_X = 0.30078125f;
+	public static final float D_Y = 0.0263671875f;
+	public static final float D_WIDTH = 0.0693359375f;
+	public static final float D_HEIGHT = 0.072265625f;
+	
+	public static final float E_X = 0.400390625f;
+	public static final float E_Y = 0.0263671875f;
+	public static final float E_WIDTH = 0.0654296875f;
+	public static final float E_HEIGHT = 0.072265625f;
+	
+	public static final float F_X = 0.5f;
+	public static final float F_Y = 0.0263671875f;
+	public static final float F_WIDTH = 0.0654296875f;
+	public static final float F_HEIGHT = 0.072265625f;
+	
+	public static final float G_X = 0.599609375f;
+	public static final float G_Y = 0.0263671875f;
+	public static final float G_WIDTH = 0.0625f;
+	public static final float G_HEIGHT = 0.072265625f;
+	
+	public static final float H_X = 0.69921875f;
+	public static final float H_Y = 0.0263671875f;
+	public static final float H_WIDTH = 0.0693359375f;
+	public static final float H_HEIGHT = 0.072265625f;
+	
+	public static final float I_X = 0.798828125f;
+	public static final float I_Y = 0.0263671875f;
+	public static final float I_WIDTH = 0.029296875f;
+	public static final float I_HEIGHT = 0.072265625f;
+	
+	public static final float J_X = 0.8984375f;
+	public static final float J_Y = 0.0263671875f;
+	public static final float J_WIDTH = 0.0419921875f;
+	public static final float J_HEIGHT = 0.072265625f;
+	
+	public static final float K_X = 0.001953125f;
+	public static final float K_Y = 0.126953125f;
+	public static final float K_WIDTH = 0.0693359375f;
+	public static final float K_HEIGHT = 0.072265625f;
+	
+	public static final float L_X = 0.1015625f;
+	public static final float L_Y = 0.126953125f;
+	public static final float L_WIDTH = 0.0625f;
+	public static final float L_HEIGHT = 0.072265625f;
+	
+	public static final float M_X = 0.201171875f;
+	public static final float M_Y = 0.126953125f;
+	public static final float M_WIDTH = 0.0771484375f;
+	public static final float M_HEIGHT = 0.072265625f;
+	
+	public static final float N_X = 0.30078125f;
+	public static final float N_Y = 0.126953125f;
+	public static final float N_WIDTH = 0.0693359375f;
+	public static final float N_HEIGHT = 0.072265625f;
+	
+	public static final float O_X = 0.400390625f;
+	public static final float O_Y = 0.126953125f;
+	public static final float O_WIDTH = 0.0625f;
+	public static final float O_HEIGHT = 0.072265625f;
+	
+	public static final float P_X = 0.5f;
+	public static final float P_Y = 0.126953125f;
+	public static final float P_WIDTH = 0.0693359375f;
+	public static final float P_HEIGHT = 0.072265625f;
+	
+	public static final float Q_X = 0.599609375f;
+	public static final float Q_Y = 0.126953125f;
+	public static final float Q_WIDTH = 0.06640625f;
+	public static final float Q_HEIGHT = 0.072265625f;
+	
+	public static final float R_X = 0.69921875f;
+	public static final float R_Y = 0.126953125f;
+	public static final float R_WIDTH = 0.0732421875f;
+	public static final float R_HEIGHT = 0.072265625f;
+	
+	public static final float S_X = 0.798828125f;
+	public static final float S_Y = 0.126953125f;
+	public static final float S_WIDTH = 0.0654296875f;
+	public static final float S_HEIGHT = 0.072265625f;
+	
+	public static final float T_X = 0.8984375f;
+	public static final float T_Y = 0.126953125f;
+	public static final float T_WIDTH = 0.0693359375f;
+	public static final float T_HEIGHT = 0.072265625f;
+	
+	public static final float U_X = 0.001953125f;
+	public static final float U_Y = 0.2255859375f;
+	public static final float U_WIDTH = 0.0693359375f;
+	public static final float U_HEIGHT = 0.072265625f;
+	
+	public static final float V_X = 0.1015625f;
+	public static final float V_Y = 0.2255859375f;
+	public static final float V_WIDTH = 0.0732421875f;
+	public static final float V_HEIGHT = 0.072265625f;
+	
+	public static final float W_X = 0.201171875f;
+	public static final float W_Y = 0.2255859375f;
+	public static final float W_WIDTH = 0.095703125f;
+	public static final float W_HEIGHT = 0.072265625f;
+	
+	public static final float X_X = 0.30078125f;
+	public static final float X_Y = 0.2255859375f;
+	public static final float X_WIDTH = 0.080078125f;
+	public static final float X_HEIGHT = 0.072265625f;
+	
+	public static final float Y_X = 0.400390625f;
+	public static final float Y_Y = 0.2255859375f;
+	public static final float Y_WIDTH = 0.076171875f;
+	public static final float Y_HEIGHT = 0.072265625f;
+	
+	public static final float Z_X = 0.5f;
+	public static final float Z_Y = 0.2255859375f;
+	public static final float Z_WIDTH = 0.0693359375f;
+	public static final float Z_HEIGHT = 0.072265625f;
+	
+	public static final float D0_X = 0.001953125f;
+	public static final float D0_Y = 0.6240234375f;
+	public static final float D0_WIDTH = 0.0625f;
+	public static final float D0_HEIGHT = 0.072265625f;
+	
+	public static final float D1_X = 0.1015625f;
+	public static final float D1_Y = 0.6240234375f;
+	public static final float D1_WIDTH = 0.0283203125f;
+	public static final float D1_HEIGHT = 0.072265625f;
+	
+	public static final float D2_X = 0.201171875f;
+	public static final float D2_Y = 0.6240234375f;
+	public static final float D2_WIDTH = 0.0625f;
+	public static final float D2_HEIGHT = 0.072265625f;
+	
+	public static final float D3_X = 0.30078125f;
+	public static final float D3_Y = 0.6240234375f;
+	public static final float D3_WIDTH = 0.05859375f;
+	public static final float D3_HEIGHT = 0.072265625f;
+	
+	public static final float D4_X = 0.400390625f;
+	public static final float D4_Y = 0.6240234375f;
+	public static final float D4_WIDTH = 0.0625f;
+	public static final float D4_HEIGHT = 0.072265625f;
+	
+	public static final float D5_X = 0.5f;
+	public static final float D5_Y = 0.6240234375f;
+	public static final float D5_WIDTH = 0.0625f;
+	public static final float D5_HEIGHT = 0.072265625f;
+	
+	public static final float D6_X = 0.599609375f;
+	public static final float D6_Y = 0.6240234375f;
+	public static final float D6_WIDTH = 0.0625f;
+	public static final float D6_HEIGHT = 0.072265625f;
+	
+	public static final float D7_X = 0.69921875f;
+	public static final float D7_Y = 0.6240234375f;
+	public static final float D7_WIDTH = 0.0625f;
+	public static final float D7_HEIGHT = 0.072265625f;
+	
+	public static final float D8_X = 0.798828125f;
+	public static final float D8_Y = 0.6240234375f;
+	public static final float D8_WIDTH = 0.0625f;
+	public static final float D8_HEIGHT = 0.072265625f;
+	
+	public static final float D9_X = 0.8984375f;
+	public static final float D9_Y = 0.6240234375f;
+	public static final float D9_WIDTH = 0.0625f;
+	public static final float D9_HEIGHT = 0.072265625f;
+	
+	public static float getX(char c)
+	{
+		switch(c)
+		{
+			case 'A':
+				return A_X;
+			case 'B':
+				return B_X;
+			case 'C':
+				return C_X;
+			case 'D':
+				return D_X;
+			case 'E':
+				return E_X;
+			case 'F':
+				return F_X;
+			case 'G':
+				return G_X;
+			case 'H':
+				return H_X;
+			case 'I':
+				return I_X;
+			case 'J':
+				return J_X;
+			case 'K':
+				return K_X;
+			case 'L':
+				return L_X;
+			case 'M':
+				return M_X;
+			case 'N':
+				return N_X;
+			case 'O':
+				return O_X;
+			case 'P':
+				return P_X;
+			case 'Q':
+				return Q_X;
+			case 'R':
+				return R_X;
+			case 'S':
+				return S_X;
+			case 'T':
+				return T_X;
+			case 'U':
+				return U_X;
+			case 'V':
+				return V_X;
+			case 'W':
+				return W_X;
+			case 'X':
+				return X_X;
+			case 'Y':
+				return Y_X;
+			case 'Z':
+				return Z_X;
+			case '0':
+				return D0_X;
+			case '1':
+				return D1_X;
+			case '2':
+				return D2_X;
+			case '3':
+				return D3_X;
+			case '4':
+				return D4_X;
+			case '5':
+				return D5_X;
+			case '6':
+				return D6_X;
+			case '7':
+				return D7_X;
+			case '8':
+				return D8_X;
+			case '9':
+				return D9_X;
+		}
+		
+		return 0.0f;
+	}
+	
+	public static float getY(char c)
+	{
+		switch(c)
+		{
+			case 'A':
+				return A_Y;
+			case 'B':
+				return B_Y;
+			case 'C':
+				return C_Y;
+			case 'D':
+				return D_Y;
+			case 'E':
+				return E_Y;
+			case 'F':
+				return F_Y;
+			case 'G':
+				return G_Y;
+			case 'H':
+				return H_Y;
+			case 'I':
+				return I_Y;
+			case 'J':
+				return J_Y;
+			case 'K':
+				return K_Y;
+			case 'L':
+				return L_Y;
+			case 'M':
+				return M_Y;
+			case 'N':
+				return N_Y;
+			case 'O':
+				return O_Y;
+			case 'P':
+				return P_Y;
+			case 'Q':
+				return Q_Y;
+			case 'R':
+				return R_Y;
+			case 'S':
+				return S_Y;
+			case 'T':
+				return T_Y;
+			case 'U':
+				return U_Y;
+			case 'V':
+				return V_Y;
+			case 'W':
+				return W_Y;
+			case 'X':
+				return X_Y;
+			case 'Y':
+				return Y_Y;
+			case 'Z':
+				return Z_Y;
+			case '0':
+				return D0_Y;
+			case '1':
+				return D1_Y;
+			case '2':
+				return D2_Y;
+			case '3':
+				return D3_Y;
+			case '4':
+				return D4_Y;
+			case '5':
+				return D5_Y;
+			case '6':
+				return D6_Y;
+			case '7':
+				return D7_Y;
+			case '8':
+				return D8_Y;
+			case '9':
+				return D9_Y;
+		}
+		
+		return 0.0f;
+	}
+	
+	public static float getWidth(char c)
+	{
+		switch(c)
+		{
+			case 'A':
+				return A_WIDTH;
+			case 'B':
+				return B_WIDTH;
+			case 'C':
+				return C_WIDTH;
+			case 'D':
+				return D_WIDTH;
+			case 'E':
+				return E_WIDTH;
+			case 'F':
+				return F_WIDTH;
+			case 'G':
+				return G_WIDTH;
+			case 'H':
+				return H_WIDTH;
+			case 'I':
+				return I_WIDTH;
+			case 'J':
+				return J_WIDTH;
+			case 'K':
+				return K_WIDTH;
+			case 'L':
+				return L_WIDTH;
+			case 'M':
+				return M_WIDTH;
+			case 'N':
+				return N_WIDTH;
+			case 'O':
+				return O_WIDTH;
+			case 'P':
+				return P_WIDTH;
+			case 'Q':
+				return Q_WIDTH;
+			case 'R':
+				return R_WIDTH;
+			case 'S':
+				return S_WIDTH;
+			case 'T':
+				return T_WIDTH;
+			case 'U':
+				return U_WIDTH;
+			case 'V':
+				return V_WIDTH;
+			case 'W':
+				return W_WIDTH;
+			case 'X':
+				return X_WIDTH;
+			case 'Y':
+				return Y_WIDTH;
+			case 'Z':
+				return Z_WIDTH;
+			case '0':
+				return D0_WIDTH;
+			case '1':
+				return D1_WIDTH;
+			case '2':
+				return D2_WIDTH;
+			case '3':
+				return D3_WIDTH;
+			case '4':
+				return D4_WIDTH;
+			case '5':
+				return D5_WIDTH;
+			case '6':
+				return D6_WIDTH;
+			case '7':
+				return D7_WIDTH;
+			case '8':
+				return D8_WIDTH;
+			case '9':
+				return D9_WIDTH;
+		}
+		
+		return 0.0f;
+	}
+	
+	public static float getHeight(char c)
+	{
+		switch(c)
+		{
+			case 'A':
+				return A_HEIGHT;
+			case 'B':
+				return B_HEIGHT;
+			case 'C':
+				return C_HEIGHT;
+			case 'D':
+				return D_HEIGHT;
+			case 'E':
+				return E_HEIGHT;
+			case 'F':
+				return F_HEIGHT;
+			case 'G':
+				return G_HEIGHT;
+			case 'H':
+				return H_HEIGHT;
+			case 'I':
+				return I_HEIGHT;
+			case 'J':
+				return J_HEIGHT;
+			case 'K':
+				return K_HEIGHT;
+			case 'L':
+				return L_HEIGHT;
+			case 'M':
+				return M_HEIGHT;
+			case 'N':
+				return N_HEIGHT;
+			case 'O':
+				return O_HEIGHT;
+			case 'P':
+				return P_HEIGHT;
+			case 'Q':
+				return Q_HEIGHT;
+			case 'R':
+				return R_HEIGHT;
+			case 'S':
+				return S_HEIGHT;
+			case 'T':
+				return T_HEIGHT;
+			case 'U':
+				return U_HEIGHT;
+			case 'V':
+				return V_HEIGHT;
+			case 'W':
+				return W_HEIGHT;
+			case 'X':
+				return X_HEIGHT;
+			case 'Y':
+				return Y_HEIGHT;
+			case 'Z':
+				return Z_HEIGHT;
+			case '0':
+				return D0_HEIGHT;
+			case '1':
+				return D1_HEIGHT;
+			case '2':
+				return D2_HEIGHT;
+			case '3':
+				return D3_HEIGHT;
+			case '4':
+				return D4_HEIGHT;
+			case '5':
+				return D5_HEIGHT;
+			case '6':
+				return D6_HEIGHT;
+			case '7':
+				return D7_HEIGHT;
+			case '8':
+				return D8_HEIGHT;
+			case '9':
+				return D9_HEIGHT;
+		}
+		
+		return 0.0f;
+	}
+}
