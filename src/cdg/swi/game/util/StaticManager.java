7e858d7c0055565d1dec109666c1d08afc5250fa
Josh Theis <cdg.josh@googlemail.com>
2013-04-02 03:04:43 +0200

    Added: ids for text uniforms

--- src/cdg/swi/game/util/StaticManager.java
+++ src/cdg/swi/game/util/StaticManager.java
@@ -4,10 +4,13 @@ public abstract class StaticManager {
 
 	public static int SELECTION_SHADER_PROGRAM_ID = 0;
 	public static int MENU_SHADER_PROGRAM_ID = 0;
+	public static int TEXT_SHADER_PROGRAM_ID = 0;
 	public static float[] CLEAR_COLOR = new float[]{0.0f,0.0f,0.4f,1.0f};
 	public static int WINDOW_WIDTH = 600;
 	public static int WINDOW_HEIGHT = 600;
 	public static int FONT_TEXTURE_ID = 0;
 	public static int FONT_TEXTURE_UNIFORM_ID = 1337;
+	public static int FONT_SCALING_MATRIX_UNIFORM_ID = 1337;
+	public static int TEXT_POSITION_UNIFORM_ID = 1337;
 
 }
