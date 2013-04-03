17b044a25e0c6a55d3cd0f198bca85b937dc8a43
Josh Theis <cdg.josh@googlemail.com>
2013-04-03 06:06:27 +0200

    added button & button functions

--- src/cdg/swi/game/menu/MainMenu.java
+++ src/cdg/swi/game/menu/MainMenu.java
@@ -1,22 +1,47 @@
 package cdg.swi.game.menu;
 
+import cdg.swi.game.util.interfaces.IClickListener;
+import cdg.swi.game.util.interfaces.IGameControl;
+
 public class MainMenu extends MenuFrame 
 {
 
-	public MainMenu()
+	public MainMenu(IGameControl c)
 	{
-		Button b = new Button(-0.5f, 0.2f);
+		super(c);
+		Button b = new Button(-0.25f, 0.2f);
 		this.add(b);
 		
-		Button b2 = new Button(0.0f, 0.0f);
+		Button b2 = new Button(-0.25f, 0.0f);
+		b2.setText("Credits");
+		b2.addClickListener(new IClickListener(){
+
+			@Override
+			public void clicked(int x, int y, int button) 
+			{
+				getControl().showCredits();
+			}
+		});
 		this.add(b2);
 		
-		Label l = new Label(-1.0f,1.0f,"TEST");
+		Button b3 = new Button(-0.25f, -0.2f);
+		b3.setText("Close");
+		b3.addClickListener(new IClickListener(){
+
+			@Override
+			public void clicked(int x, int y, int button) 
+			{
+				System.exit(1337);
+			}
+		});
+		this.add(b3);
+		
+		Label l = new Label(-1.0f,1.0f,"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 		this.add(l);
 	}
 	
 	public void setLabelText(String text)
 	{
-		((Label)this.getComponentById(3)).setText(text);
+		((Label)this.getComponentById(4)).setText(text);
 	}
 }
