63844547be8ffd1b1d5402c742225059bb276801
Josh Theis <cdg.josh@googlemail.com>
2013-04-05 06:26:51 +0200

    WIP: new Layout

--- src/cdg/swi/game/menu/MainMenu.java
+++ src/cdg/swi/game/menu/MainMenu.java
@@ -1,20 +1,65 @@
 package cdg.swi.game.menu;
 
+import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
+import static org.lwjgl.opengl.GL15.glBindBuffer;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+
+import cdg.swi.game.util.StaticManager;
+import cdg.swi.game.util.Utility;
+import cdg.swi.game.util.VertexData;
 import cdg.swi.game.util.interfaces.IClickListener;
 import cdg.swi.game.util.interfaces.IGameControl;
 
 public class MainMenu extends MenuFrame 
 {
+	Button newGame;
+	Button campaign;
+	Button stats;
+	Button options;
+	Button credits;
+	Button close;
+	
+	Label headLabel;
+	Label titleLabel;
 
 	public MainMenu(IGameControl c)
 	{
 		super(c);
-		Button b = new Button(-0.25f, 0.2f);
-		this.add(b);
 		
-		Button b2 = new Button(-0.25f, 0.0f);
-		b2.setText("Credits");
-		b2.addClickListener(new IClickListener(){
+		this.newGame = new Button(-0.97142857143f, -0.8f);
+		this.newGame.setTextSize(0.7f);
+		this.newGame.setText("new game");
+		this.add(this.newGame);
+		
+		this.campaign = new Button(-0.64285714286f, -0.8f);
+		this.campaign.setTextSize(0.7f);
+		this.campaign.setText("campaign");
+		this.add(this.campaign);
+		
+		this.stats = new Button(-0.31428571429f, -0.8f);
+		this.stats.setTextSize(0.7f);
+		this.stats.setText("stats");
+		this.add(this.stats);
+		
+		this.options = new Button(0.01428571428f, -0.8f);
+		this.options.setTextSize(0.7f);
+		this.options.setText("options");
+		this.add(this.options);
+		
+		this.credits = new Button(0.34285714285f, -0.8f);
+		this.credits.setTextSize(0.7f);
+		this.credits.setText("credits");
+		this.credits.addClickListener(new IClickListener(){
 
 			@Override
 			public void clicked(int x, int y, int button) 
@@ -22,26 +67,30 @@ public class MainMenu extends MenuFrame
 				getControl().showCredits();
 			}
 		});
-		this.add(b2);
+		this.add(this.credits);
 		
-		Button b3 = new Button(-0.25f, -0.2f);
-		b3.setText("Close");
-		b3.addClickListener(new IClickListener(){
+		this.close = new Button(0.67142857142f, -0.8f);
+		this.close.setTextSize(0.7f);
+		this.close.setText("close");
+		this.close.addClickListener(new IClickListener(){
 
 			@Override
 			public void clicked(int x, int y, int button) 
 			{
 				System.exit(1337);
-			}
-		});
-		this.add(b3);
+		}});
+		this.add(this.close);
 		
-		Label l = new Label(-1.0f,1.0f,"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-		this.add(l);
+		this.headLabel = new Label(-0.6f, 0.8f, "space empires");
+		this.headLabel.setTextSize(2.5f);
+		this.headLabel.setTextColor(1.0f, 1.0f, 1.0f, 0.1f);
+		this.add(this.headLabel);
+		
+		this.setDrawBackground(true);
 	}
 	
 	public void setLabelText(String text)
 	{
-		((Label)this.getComponentById(4)).setText(text);
+		//((Label)this.getComponentById(5)).setText(text);
 	}
 }
