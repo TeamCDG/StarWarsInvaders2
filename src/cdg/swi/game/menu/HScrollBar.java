15650ff327ba00923ad69d3731c68be1f93bffb0
Josh Theis <cdg.josh@googlemail.com>
2013-04-12 01:46:48 +0200

    WIP: Scrolling

---
+++ src/cdg/swi/game/menu/HScrollBar.java
@@ -0,0 +1,22 @@
+package cdg.swi.game.menu;
+
+public class HScrollBar extends Component {
+
+	public HScrollBar(float x, float y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setAlpha(float alpha) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void drawUI() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
