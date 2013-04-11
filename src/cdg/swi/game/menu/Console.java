f48c784ffc029ad9b6a14c741efc0c58e25588b9
Josh Theis <cdg.josh@googlemail.com>
2013-04-12 01:48:22 +0200

    WIP: dev console

---
+++ src/cdg/swi/game/menu/Console.java
@@ -0,0 +1,14 @@
+package cdg.swi.game.menu;
+
+public class Console extends Overlay {
+
+	public Console() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Console(float alpha) {
+		super(alpha);
+		// TODO Auto-generated constructor stub
+	}
+
+}
